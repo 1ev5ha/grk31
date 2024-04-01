e5e4af39ace698e7af9c90e54de691b0dedd9ccb
LEV5HA <90126701+1ev5ha@users.noreply.github.com>
2024-04-01 23:55:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*document.querySelectorAll(".accordion-header").forEach(header =>
+    {
+        header.addEventListener("click", () => {
+            const accordionItem = header.parentNode;
+            const content = accordionItem.querySelector(".accordion-content");
+            accordionItem.classList.toggle("active");
+            content.style.maxHeight = accordionItem.classList.contains("active") ? content.scrollHeight + "px" : null;
+
+            document.querySelectorAll(".accordion-item").forEach
+            (otherAccordionItem => {
+                if(otherAccordionItem !== accordionItem){
+                    otherAccordionItem.classList.remove("active");
+                    otherAccordionItem.querySelector(".accordion-content").style.maxHeight = null;
+                }
+            });
+        });
+    });*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function()
+    {
+        document.querySelector(".header").classList.toggle("open")
+    })
+})
